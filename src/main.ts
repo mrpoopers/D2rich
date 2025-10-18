ea960168d21eeeaf4342adff2b507e8518a37379
mrpoopers <rnimnual@ucsc.edu>
2025-10-18 19:05:53 +0000

    Step One, added canvas, title, and edited aspects of canvs.

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,6 @@ import "./style.css";
 
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+   <h1 class="title">SketchPad</h1>
+   <canvas id="myCanvas" width="600" height="400"></canvas>
 `;
