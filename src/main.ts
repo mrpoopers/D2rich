c8965a11a8b0434bc57b59496f12f0fa2901135f
mrpoopers <rnimnual@ucsc.edu>
2025-10-18 20:10:09 +0000

    Step 2, added drawing functionality and theh ability to clear the canvas.

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,34 @@ document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
    <h1 class="title">SketchPad</h1>
    <canvas id="myCanvas" width="600" height="400"></canvas>
+  <button type="button" id="clearBtn">Clear</button>
 `;
+
+let drawing = false;
+
+const canvas = document.getElementById("myCanvas") as HTMLCanvasElement | null;
+const ctx = canvas?.getContext("2d");
+
+if (canvas && ctx) {
+  canvas.addEventListener("mousedown", (e) => {
+    drawing = true;
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX, e.offsetY);
+
+    canvas.addEventListener("mousemove", (e) => {
+      if (!drawing) return;
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+    });
+
+    canvas.addEventListener("mouseup", () => drawing = false);
+    canvas.addEventListener("mouseleave", () => drawing = false);
+  });
+}
+
+const clearBtn = document.getElementById("clearBtn");
+if (clearBtn && canvas && ctx) {
+  clearBtn.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  });
+}
